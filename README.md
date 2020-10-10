3f4e198ff5712286b4caea054eeccc657e9534b9
Elgar Teo <elgarteo95@gmail.com>
2020-10-10 22:32:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# 香港政治詞庫 Hong Kong Politics Dictionary
+本詞庫收錄與香港政治相關的常用關鍵字，以方便進行相關主題的文本勘探工作。
+
+A dictionary containing commonly used keywords relating to Hong Kong politics.
+Good for text mining or other automated text processing applications.
+
+## 使用方法
+
+本詞庫由9個文本檔案所組成，分別收錄以下各個類別的常用詞：
+
+* **government**: 各政府部門以及職位
+* **lawmaker**: 現屆和歷任立法會及區議會議員
+* **media**: 傳媒
+* **official**: 現屆和歷任主要政府官員
+* **organisation**: 各大公營機構、政黨及政治組織
+* **person**: 著名人士
+* **place**: 地名/街道
+* **policy**: 政策/法案
+* **slang**: 政治術語/俚語
+* **misc**: 其他未能分類之常用詞
+
+如需合併詞庫成單一檔案（如匯入jieba作斷詞），可使用`merge.sh`自由合併檔案。
+
+本詞庫仍在擴充階段，歡迎任何人士提交pull request。
+
+## 免責聲明
+
+本詞庫之收錄詞、排序及分類方法並無任何宣傳或貶低任何組織、人士或立場之意。如認為有不當之處，歡迎在Issues提出。
---
+++ merge.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+files=(government lawmaker media misc official organisation person place policy slang)
+echo "===============Hong Kong Politics Dictionary==============="
+for ((i=0; i<${#files[@]}; i++)); do
+        echo "[$((i + 1))] ${files[${i}]}"
+done
+echo "==> File(s) to merge: (\"0\" to merge all) (eg: \"1\" \"1 5 7\")"
+printf "==> "
+read target
+if [ "$target" = "0" ]
+then
+  delim=""
+  joined=""
+  for file in "${files[@]}"; do
+    joined="$joined$delim$file.txt"
+    delim=" "
+  done
+  cat $joined > merged.txt
+  echo "All files succesfully merged as 'merged.txt'"
+else
+  IFS=" " read -r -a targets <<< "$target"
+  delim=""
+  for x in "${targets[@]}"; do
+    if ((x < 1 || x > 10))
+    then
+      echo "Error: Invalid index"
+      exit
+    fi
+    joined="$joined$delim${files[$((x - 1))]}.txt"
+    delim=" "
+  done
+  cat $joined > merged.txt
+  echo "Selected files succesfully merged as 'merged.txt'"
+fi
